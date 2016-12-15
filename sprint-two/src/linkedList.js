0047804b04d9f95bd8e2e824a34cdd5e985330ef
simon-huang <simon.t.huang@gmail.com>
2016-12-14 21:03:52 -0800

    Pass linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,40 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  list.count = 0;
+
   list.addToTail = function(value) {
+    var newTail = Node(value); 
+    if (list.head === null) {
+      list.head = newTail;
+    }
+    if (list.count === 1) {
+      list.head.next = newTail;
+    } else if (list.count > 1) {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
+    list.count++;
+    console.log(list.head);
   };
 
   list.removeHead = function() {
+    var currentHeadValue = list.head.value;
+    list.head = list.head.next;
+    list.count--;
+    return currentHeadValue;
+    console.log(list.head);
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    for (var i = 0; i < list.count; i++) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = list.head.next;
+    }
+    return false;
   };
 
   return list;
@@ -23,7 +50,3 @@ var Node = function(value) {
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
