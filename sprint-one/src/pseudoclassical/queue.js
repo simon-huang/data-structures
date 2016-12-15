1be7587892b9142ad2c2a1f9e2811b2c3157a85d
simon-huang <simon.t.huang@gmail.com>
2016-12-14 16:27:22 -0800

    Pass all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+  this.storage = {};
+  this.index = 0;
+
 };
 
+
+
+Queue.prototype['enqueue'] = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype['dequeue'] = function() {
+
+  if (this.index > 0) {
+    this.index--;
+    var result = this.storage[0];
+    for (var i = 0; i < this.index; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    return result;
+  } 
+};
+
+Queue.prototype['size'] = function() {
+  return this.index;
+};
 
